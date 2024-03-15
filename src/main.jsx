4c732c6f2abe9002a172d5a8d8c4be40ce3c5070
JAIATEL <jainampatel2509@gmail.com>
2024-03-15 18:15:21 +0530

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Services from "./Pages/Services.jsx";
+import Product from "./Pages/Product.jsx";
+import Solution from "./Pages/Solution.jsx";
+import Contact from "./Pages/Contact.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      {
+        path: "/product",
+        element: <Product />,
+      },
+      {
+        path: "/solution",
+        element: <Solution />,
+      },
+      {
+        path : "/contact",
+        element : <Contact />
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
